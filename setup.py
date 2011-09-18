376336390d86ff283f1c698c4a0b603371b71b4f
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2011-09-18 17:24:31 +0600

    Some fix

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='django-easecontacts',
+    name='django-easycontacts',
     version='0.1',
     keywords='django, application, contacts',
     author='Aleksandr Aibulatov',
